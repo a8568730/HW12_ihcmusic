a0f946170be1906b45feb4dade3e2566816f2ae6
a8568730 <a8568730@gmail.com.tw>
2014-05-21 23:34:28 +0800

    筆記了如何快速新增一個點，並能指定它自己的css屬性

--- HW9_pattern2/js/0515_2.js
+++ HW9_pattern2/js/0515_2.js
@@ -1,17 +1,22 @@
 $(function(){
-    addarrow();
-    setsize();
     var step=$('.box').width();
     var posArray=[];
     var numArray=[];    
     
+    //preset
+    addarrow();
+    setsize();
     $('.content').each(function(){
         var index = $(this).index();
         var num = $(this).find('.box').length;
         numArray.push(num);
         posArray.push(0);
     });
-        
+    
+    //json
+    getcon('js/list4.json');
+    
+    //move
     $('.content').on('click','.left',function(){
         var $content = $(this).parent();
         var index = $content.index();
@@ -24,11 +29,10 @@ $(function(){
             posArray[index-1] = pos;
         }
     });
-     $('.content').on('click','.right',function(){
+    $('.content').on('click','.right',function(){
         var $content = $(this).parent();
         var index = $content.index();
         var pos = posArray[index-1];
-//         alert(pos +"," + numArray[index-1]);
         if(pos < (numArray[index-1]-4)){
             $content.find('.out')                  
                     .find('.container')
@@ -37,7 +41,8 @@ $(function(){
             posArray[index-1] = pos;
         }
     });
-    //$('.container').css({top:0,left:"0px"});
+    
+    //pick pattern 
     $('.box').on('click','div',function(){
         var src =  $(this).css('backgroundColor');
         $('body').css('backgroundColor',src);
@@ -58,3 +63,41 @@ function setsize(){
     var num = $('.container').data('num');
     $('.container').width(w*(num+1));
 };
+
+
+function getcon(filename){
+	$.getJSON(filename, function(data){
+			
+		var html = ' <div class="content"> '
+					+ '<div class="out">'
+						+ '<div class="container">';
+
+		$.each(data, function(index, en){
+			//html += '<div class="pic" id="pic' + index + '"></div>';
+			//html += '<div class="text">';
+			//html += '<h2>' + en['title'] + '</h2>';
+			//html += '<p>' + en['author'] + '</p>';
+			//html += '<button class="btn view">View</button>';
+			//html += '</div></div>';
+			html += '<div class="box">';
+			html += '<img src="' + en['pic'] + '"/>';
+            html += '</div>';
+			/*a=$('<div>');
+			a.css('cc',5);
+			b=$('<div>');
+			b.css('cc',6);*/
+            /*
+             * 
+             *<div></div> -> css.backgroudnImage p1, append
+            <div></div> -> backgroudnImage p2, append
+            */
+			//$('#pic' + index).css({'backgroundImage':'url(' + en['pic'] + ')',
+				//				   'backgroundColor':'#fff'
+					//			 });
+			
+		});
+		$('#wrapper').append(html);
+		
+		return false;
+	});
+};	
