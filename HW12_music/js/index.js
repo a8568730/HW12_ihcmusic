788b05bc0de7d5dd7e35064ce645f0a26c8227f2
a8568730 <a8568730@gmail.com.tw>
2014-06-08 23:48:32 +0800

    目前還在做手機的版型 文莉

---
+++ HW12_music/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	/*$("#head-container").find("input").focus(function(){
+		var text = $(this).val();
+		$(this).css('background-image','none');
+		if(text != '')
+		{
+			$(this).css('background-image','none');
+		}
+	});*/
+	
+	
+});
