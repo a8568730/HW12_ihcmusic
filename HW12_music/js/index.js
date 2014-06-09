384956e6f7ee477242a63bc02b7934828fa9bdd7
wenli <a8568730@hotmail.com>
2014-06-09 15:56:39 +0800

    加入第二頁網頁，並且附加換頁元素與換頁效果

--- HW12_music/js/index.js
+++ HW12_music/js/index.js
@@ -7,11 +7,22 @@ $(document).ready(function(){
 			$(this).css('background-image','none');
 		}
 	});*/
+	var window = $("#window");
+	var wrapper = $("#wrapper");
 	
 	$("#ss-container").find("button").on('click',function(){
 		$(this).toggleClass("btn-active");
 		//var signcontain = $(this).closest("#ss-container").next();
-		var signcontain = $('#sign-group');
+		var signcontain = $("#sign-group");
 		signcontain.slideToggle('fast');
+	});
+	
+	$("#head-nav").find("a").on('click',function(){
+		var h = window.height();
+		wrapper.offset({'top':-h});
+	});
+	
+	$("#return").on('click',function(){
+		wrapper.offset({'top':0});
 	});
 });
